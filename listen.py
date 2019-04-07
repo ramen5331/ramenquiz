49a3374f7349c87d508b7760b582dd52103444bc
xt <xiuting91@gmail.com>
2019-04-08 01:55:52 +0800

    test

--- listen.py
+++ listen.py
@@ -20,14 +20,14 @@ while 1:
 	l.append(c)
 	print ('%d: connection from %s' % (len(l), a))
 	filename = '/index.html'
-    f = open(filename, 'r')
-    c.sendall(str.encode("HTTP/1.0 200 OK\n",'iso-8859-1'))
-    c.sendall(str.encode('Content-Type: text/html\n', 'iso-8859-1'))
-    c.send(str.encode('\r\n'))
-    # send data per line
-    for l in f.readlines():
-        print('Sent ', repr(l))
-        c.sendall(str.encode(""+l+"", 'iso-8859-1'))
-        l = f.read(1024)
-    f.close()
-    c.close()
+	f = open(filename, 'r')
+	c.sendall(str.encode("HTTP/1.0 200 OK\n",'iso-8859-1'))
+	c.sendall(str.encode('Content-Type: text/html\n', 'iso-8859-1'))
+	c.send(str.encode('\r\n'))
+	# send data per line
+	for l in f.readlines():
+		print('Sent ', repr(l))
+		c.sendall(str.encode(""+l+"", 'iso-8859-1'))
+		l = f.read(1024)
+	f.close()
+	c.close()
