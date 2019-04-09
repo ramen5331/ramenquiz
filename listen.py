bff3a5d3d732b296822895e253b8eae0c6b65ae1
xt <xiuting91@gmail.com>
2019-04-10 00:32:59 +0800

    testnn

--- listen.py
+++ listen.py
@@ -19,16 +19,3 @@ while 1:
 	conn, (c, a) = s.accept()
 	l.append(c)
 	print ('%d: connection from %s' % (len(l), a))
-
-import web
-urls = (
-'/input', 'index'
-)
-class index:
-	def GET(self):
-		i = web.input(name=None)
-		return render.index(i.name)
-if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
-render = web.template.render('/')
